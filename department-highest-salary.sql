46ebff22826cd3b18504f4f71a149bfd8eeca08f
Kumaresan Rasakumar (BT: Product Dev. & Eng.: CoE Engineering) <Rasakumar.Kumaresan@hm.com>
2025-07-31 18:39:52 +0530

    Added Solution

--- department-highest-salary.sql
+++ department-highest-salary.sql
@@ -1,7 +1,4 @@
 /* 
-
-# Time:  O(n^2)
-# Space: O(n)
 # 
 # The Employees table holds all employees. 
 Every employee has an Id, a salary, and there is also a column for the department Id.
@@ -54,20 +51,18 @@ CREATE TABLE Employees (
 
 -- Insert Sample Data
 -- Insert into Department
-
 INSERT INTO Department (Id, Name) VALUES
 (1, 'IT'),
 (2, 'Sales');
 
 -- Insert into Employee
-
 INSERT INTO Employees (Id, Name, Salary, DepartmentId) VALUES
 (1, 'Joe', 70000, 1),
 (2, 'Henry', 80000, 2),
 (3, 'Sam', 60000, 2),
 (4, 'Max', 90000, 1);
 
--- Solution to Department Higest Salary
+-- Solution to Department Highest Salary
 -- Using Joins
 SELECT 
     d.Name AS Department,
